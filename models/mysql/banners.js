c69447028420393eea3621b8286eead615c3509c
slayermass <slayermass@mail.ru>
2018-06-29 16:46:22 +0700

    sync

--- models/mysql/banners.js
+++ models/mysql/banners.js
@@ -96,7 +96,7 @@ model.infoBannerPublic = async (params) => {
     return new Promise((resolve, reject) => {
         mysql
             .getSqlQuery("SELECT `script_banner`, `id_banner`, `pos_banner` FROM `" + TABLE_NAME + "` " +
-                " WHERE `fk_site` = :fk_site AND `date_since` >= CURDATE() AND `date_to` <= CURDATE();", {
+                " WHERE `fk_site` = :fk_site AND `date_since` >= CURDATE() AND `date_to` >= CURDATE();", {
                 fk_site     : params.fk_site
             })
             .then(rows => {
